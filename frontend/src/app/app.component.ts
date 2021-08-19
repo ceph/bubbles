56be9d185c57756d76f48761b127f0375c41ed10
Joao Eduardo Luis <joao@suse.com>
2021-08-19 16:07:40 +0000

    frontend: add basic angular app
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bubbles-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bubbles';
+}
