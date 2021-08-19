1f8c631ae8cdf95cb653d1b9888ff084f6d7625c
Joao Eduardo Luis <joao@suse.com>
2021-08-19 15:08:21 +0000

    bubbles: add basic mgr module
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

---
+++ module.py
@@ -0,0 +1,55 @@
+# bubbles - a simplified management UI for Ceph
+# Copyright (C) 2021 SUSE, LLC
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# pyright: reportMissingTypeStubs=false, reportUnknownMemberType=false
+from typing import Any, Optional
+import uvicorn
+from fastapi import FastAPI
+
+from mgr_module import MgrModule
+
+
+class BubblesModule(MgrModule):
+    MODULE_OPTIONS = []
+    NATIVE_OPTIONS = []
+
+    app: Optional[FastAPI] = None
+    api: Optional[FastAPI] = None
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+    async def _startup(self) -> None:
+        self.log.info("Startup Bubbles")
+        assert self.api
+
+    async def _shutdown(self) -> None:
+        self.log.info("Shutdown Bubbles")
+        assert self.api
+
+    def serve(self) -> None:
+        self.log.info("Starting Bubbles server")
+        self.app = FastAPI(docs_url=None)
+        self.api = FastAPI(
+            title="Bubbles is a simplified management UI for Ceph"
+        )
+        self.app.add_event_handler("startup", self._startup)
+        self.app.add_event_handler("shutdown", self._shutdown)
+
+        self.app.mount("/api", self.api, name="api")
+        uvicorn.run(self.app, host="0.0.0.0", port=1337)  # type: ignore
+
+    def shutdown(self) -> None:
+        self.log.info("Shutting down Bubbles server")
+        if not self.app:
+            return
