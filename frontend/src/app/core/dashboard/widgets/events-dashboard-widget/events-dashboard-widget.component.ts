3c2e9698991e5e152991489cdd4792e094f89ca4
Volker Theile <vtheile@suse.com>
2021-10-04 19:01:59 +0200

    frontend: Use pipe to render the date column
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- frontend/src/app/core/dashboard/widgets/events-dashboard-widget/events-dashboard-widget.component.ts
+++ frontend/src/app/core/dashboard/widgets/events-dashboard-widget/events-dashboard-widget.component.ts
@@ -3,6 +3,7 @@ import { marker as TEXT } from '@biesbjerg/ngx-translate-extract-marker';
 import { Observable } from 'rxjs';
 
 import { DatatableColumn } from '~/app/shared/models/datatable-column.type';
+import { RelativeDatePipe } from '~/app/shared/pipes/relative-date.pipe';
 import { ClusterService, Event } from '~/app/shared/services/api/cluster.service';
 
 @Component({
@@ -12,22 +13,25 @@ import { ClusterService, Event } from '~/app/shared/services/api/cluster.service
 })
 export class EventsDashboardWidgetComponent {
   data: Event[] = [];
-  columns: DatatableColumn[] = [
-    {
-      name: TEXT('Date'),
-      prop: 'ts'
-    },
-    {
-      name: TEXT('Severity'),
-      prop: 'severity'
-    },
-    {
-      name: TEXT('Message'),
-      prop: 'message'
-    }
-  ];
+  columns: DatatableColumn[];
 
-  constructor(private clusterService: ClusterService) {}
+  constructor(private clusterService: ClusterService, private relativeDatePipe: RelativeDatePipe) {
+    this.columns = [
+      {
+        name: TEXT('Date'),
+        prop: 'ts',
+        pipe: this.relativeDatePipe
+      },
+      {
+        name: TEXT('Severity'),
+        prop: 'severity'
+      },
+      {
+        name: TEXT('Message'),
+        prop: 'message'
+      }
+    ];
+  }
 
   updateData($data: Event[]) {
     this.data = $data;
