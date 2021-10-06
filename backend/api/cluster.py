ceph/bubbles

commit 24a1449189e5b8b972d95a6131dea2360c0bf4eb

backend/frontend: Dashboard widgets
- Add /api/cluster/status, /api/cluster/client-io-rates and /api/cluster/events API endpoints
- Adapt health widget
- Rename SystemStatusService to ClusterStatusService
- Remove the /api/status endpoint. The data can be received via the /api/cluster/status endpoint
- Add Add events widget

Signed-off-by: Volker Theile <vtheile@suse.com>

backend/api/cluster.py
@@ -5,12 +5,13 @@
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
 #
-from typing import Callable
+from typing import Callable, List
 
 from fastapi import APIRouter, Depends, Request
 
 from bubbles.bubbles import Bubbles
 from bubbles.backend.api import jwt_auth_scheme
+from bubbles.backend.models.cluster import ClusterStatusModel, EventModel
 from bubbles.backend.models.df import ClusterUsageStatsModel
 
 router = APIRouter(prefix="/cluster", tags=["cluster"])
@@ -23,3 +24,32 @@ async def get_df(
     bubbles: Bubbles = request.app.state.bubbles
     assert bubbles.ctrls.cluster is not None
     return bubbles.ctrls.cluster.df()
+
+
+@router.get(
+    "/status",
+    name="Get the cluster status information",
+    response_model=ClusterStatusModel
+)
+async def get_status(
+    request: Request, _: Callable = Depends(jwt_auth_scheme)
+) -> ClusterStatusModel:
+    bubbles: Bubbles = request.app.state.bubbles
+    assert bubbles.ctrls.cluster is not None
+    return bubbles.ctrls.cluster.status()
+
+
+@router.get("/events", response_model=List[EventModel])
+async def get_events(
+    request: Request, _: Callable = Depends(jwt_auth_scheme)
+) -> List[EventModel]:
+    events = [{
+        "ts": 1633362463,
+        "severity": "info",
+        "message": "fooo bar asdasdlkasjd aksdjlas dasjdlsakjd asdkasld asdas."
+    }, {
+        "ts": 1633363417,
+        "severity": "warn",
+        "message": "Lorem ipsum dolor sit amet, sed diam voluptua."
+    }]
+    return [EventModel.parse_obj(event) for event in events]

backend/controllers/cluster.py
@@ -5,8 +5,12 @@
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
 #
-from mgr_module import MgrModule
+from typing import Any, Dict
+
+from bubbles.backend.models.cluster import ClusterStatusModel
 from bubbles.backend.models.df import ClusterUsageStatsModel
+
+from mgr_module import MgrModule
 
 
 class ClusterController:
@@ -14,6 +18,12 @@ class ClusterController:
 
     def __init__(self, mgr: MgrModule) -> None:
         self._mgr = mgr
+
+    def status(self) -> ClusterStatusModel:
+        cmd: Dict[str, Any] = {"prefix": "status", "format": "json"}
+        _, out, _ = self._mgr.mon_command(cmd)
+        status = ClusterStatusModel.parse_raw(out)
+        return status
 
     def df(self) -> ClusterUsageStatsModel:
         return ClusterUsageStatsModel.get(self._mgr)

backend/models/cluster.py
@@ -1,0 +1,106 @@
+# Copyright (C) 2021 SUSE, LLC
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+from typing import Any, Dict, List
+
+from pydantic import BaseModel, Field
+
+
+class OSDPoolEntryModel(BaseModel):
+    pool: int
+    pool_name: str
+    size: int
+    min_size: int
+    crush_rule: int
+
+
+class OSDMapModel(BaseModel):
+    epoch: int
+    num_osds: int
+    num_up_osds: int
+    osd_up_since: int
+    num_in_osds: int
+    osd_in_since: int
+    num_remapped_pgs: int
+
+
+class HealthCheckSummaryModel(BaseModel):
+    message: str
+    count: int
+
+
+class HealthCheckModel(BaseModel):
+    severity: str
+    summary: HealthCheckSummaryModel
+    muted: bool
+
+
+class HealthStatusModel(BaseModel):
+    status: str
+    checks: Dict[str, HealthCheckModel]
+
+
+class PGStateModel(BaseModel):
+    state_name: str
+    count: int
+
+
+class PGMapModel(BaseModel):
+    pgs_by_state: List[PGStateModel]
+    num_pgs: int = Field(0, title="Number of placement groups")
+    num_pools: int = Field(0, title="Number of pools")
+    num_objects: int = Field(0, title="Number of objects")
+    # storage statistics
+    data_bytes: int = Field(0, title="Total data (byte)")
+    bytes_used: int = Field(0, title="Used storage (byte)")
+    bytes_avail: int = Field(0, title="Available storage (byte)")
+    bytes_total: int = Field(0, title="Total storage (byte)")
+    # pg statistics
+    inactive_pgs_ratio: float = Field(0, title="Ratio of inactive PGs")
+    degraded_objects: int = Field(0, title="Number of degraded objects")
+    degraded_total: int = Field(0, title="Total number of degraded copies")
+    degraded_ratio: float = Field(0, title="Ratio of degraded objects")
+    # client io
+    read_bytes_sec: int = Field(0, title="Client reads per second (byte)")
+    write_bytes_sec: int = Field(0, title="Client writes per second (byte)")
+    read_op_per_sec: int = Field(0, title="Client read operations per second")
+    write_op_per_sec: int = Field(0, title="Client write operations per second")
+
+
+class MONMapModel(BaseModel):
+    epoch: int
+    min_mon_release_name: str
+    num_mons: int
+
+
+class MGRMapModel(BaseModel):
+    available: bool
+    num_standbys: int
+    modules: List[str]
+    services: Dict[str, Any]
+
+
+class ClusterStatusModel(BaseModel):
+    fsid: str
+    election_epoch: int
+    quorum: List[int]
+    quorum_names: List[str]
+    quorum_age: int
+    health: HealthStatusModel
+    monmap: MONMapModel
+    osdmap: OSDMapModel
+    pgmap: PGMapModel
+    fsmap: Dict[str, Any]
+    mgrmap: MGRMapModel
+    servicemap: Dict[str, Any]
+    progress_events: Dict[str, Any]
+
+
+class EventModel(BaseModel):
+    ts: int = Field(title="The Unix time stamp")
+    severity: str
+    message: str

backend/api/status.py
@@ -1,25 +1,0 @@
-# Copyright (C) 2021 SUSE, LLC
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-from typing import Callable
-
-from fastapi import APIRouter, Depends, Request
-
-from bubbles.bubbles import Bubbles
-from bubbles.backend.api import jwt_auth_scheme
-from bubbles.backend.models.status import StatusModel
-
-router = APIRouter(prefix="/status", tags=["status"])
-
-
-@router.get("/", name="Get the status information", response_model=StatusModel)
-async def get_status(
-    request: Request, _: Callable = Depends(jwt_auth_scheme)
-) -> StatusModel:
-    bubbles: Bubbles = request.app.state.bubbles
-    assert bubbles.ctrls.rest_api_proxy is not None
-    return StatusModel(dashboard_url=bubbles.ctrls.rest_api_proxy.base_url)

backend/models/status.py
@@ -1,12 +1,0 @@
-# Copyright (C) 2021 SUSE, LLC
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-from pydantic import BaseModel, Field
-
-
-class StatusModel(BaseModel):
-    dashboard_url: str = Field(title="The URL to the Ceph Dashboard")

frontend/src/app/app-routing.module.ts
@@ -16,37 +16,40 @@ import { NotFoundPageComponent } from '~/app/pages/not-found-page/not-found-page
 import { ServicesPageComponent } from '~/app/pages/services-page/services-page.component';
 import { UsersPageComponent } from '~/app/pages/users-page/users-page.component';
 import { DialogComponent } from '~/app/shared/components/dialog/dialog.component';
+import { ClusterStatus } from '~/app/shared/services/api/cluster.service';
 import { AuthGuardService } from '~/app/shared/services/auth-guard.service';
+import { ClusterStatusService } from '~/app/shared/services/cluster-status.service';
 import { DialogService } from '~/app/shared/services/dialog.service';
-import { SystemStatus, SystemStatusService } from '~/app/shared/services/system-status.service';
 
 @Injectable()
 export class CephDashboardRedirectResolver implements Resolve<any> {
   constructor(
     private dialogService: DialogService,
-    private systemStatusService: SystemStatusService
+    private clusterStatusService: ClusterStatusService
   ) {}
 
   resolve(route: ActivatedRouteSnapshot): any {
     const url = decodeURIComponent(route.paramMap.get('url')!);
     if (_.isString(url)) {
-      this.systemStatusService.systemStatus$
-        .pipe(first())
-        .subscribe((systemStatus: SystemStatus) => {
-          this.dialogService.open(
-            DialogComponent,
-            (res: boolean) => {
-              if (res) {
-                window.open(`${systemStatus.dashboard_url}/#${url}`, '_blank');
-              }
-            },
-            {
-              type: 'okCancel',
-              icon: 'info',
-              message: TEXT('This will redirect you to the Ceph Dashboard.')
+      this.clusterStatusService.status$.pipe(first()).subscribe((clusterStatus: ClusterStatus) => {
+        const dashboardUrl: string | null = _.get(clusterStatus, 'mgrmap.services.dashboard', null);
+        if (!_.isString(dashboardUrl)) {
+          return;
+        }
+        this.dialogService.open(
+          DialogComponent,
+          (res: boolean) => {
+            if (res) {
+              window.open(`${_.trimEnd(dashboardUrl, '/')}/#${url}`, '_blank');
             }
-          );
-        });
+          },
+          {
+            type: 'okCancel',
+            icon: 'info',
+            message: TEXT('This will redirect you to the Ceph Dashboard.')
+          }
+        );
+      });
     }
     return EMPTY;
   }

frontend/src/app/core/dashboard/dashboard.module.ts
@@ -6,21 +6,21 @@ import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
+import { EventsDashboardWidgetComponent } from '~/app/core/dashboard/widgets/events-dashboard-widget/events-dashboard-widget.component';
 import { HealthDashboardWidgetComponent } from '~/app/core/dashboard/widgets/health-dashboard-widget/health-dashboard-widget.component';
 import { PerformanceDashboardWidgetComponent } from '~/app/core/dashboard/widgets/performance-dashboard-widget/performance-dashboard-widget.component';
 import { SysInfoDashboardWidgetComponent } from '~/app/core/dashboard/widgets/sys-info-dashboard-widget/sys-info-dashboard-widget.component';
-import { VolumesDashboardWidgetComponent } from '~/app/core/dashboard/widgets/volumes-dashboard-widget/volumes-dashboard-widget.component';
 import { SharedModule } from '~/app/shared/shared.module';
 
 @NgModule({
   declarations: [
-    VolumesDashboardWidgetComponent,
+    EventsDashboardWidgetComponent,
     HealthDashboardWidgetComponent,
     SysInfoDashboardWidgetComponent,
     PerformanceDashboardWidgetComponent
   ],
   exports: [
-    VolumesDashboardWidgetComponent,
+    EventsDashboardWidgetComponent,
     HealthDashboardWidgetComponent,
     SysInfoDashboardWidgetComponent,
     PerformanceDashboardWidgetComponent

frontend/src/app/core/dashboard/widgets/events-dashboard-widget/events-dashboard-widget.component.html
@@ -1,10 +1,10 @@
 <cb-widget [loadData]="loadData.bind(this)"
-           title="Volumes"
+           title="{{ 'Events' | translate }}"
            (loadDataEvent)="updateData($event)">
-  <div class="cb-volumes-dashboard-widget"
+  <div class="cb-events-dashboard-widget"
        fxLayout="row"
        fxLayoutAlign="center center">
-    <div class="cb-volumes-dashboard-widget-table"
+    <div class="cb-events-dashboard-widget-table"
          fxFlex
          fxLayout="column">
       <cb-datatable [data]="data"

commit 36197841ffa7aa3ff9ac7c638ea44ae3e8d84ac0

bubbles: apply changes from python black
fixes lint issues introduced by 24a14491

Signed-off-by: Michael Fritch <mfritch@suse.com>

backend/api/cluster.py
@@ -29,7 +29,7 @@ async def get_df(
 @router.get(
     "/status",
     name="Get the cluster status information",
-    response_model=ClusterStatusModel
+    response_model=ClusterStatusModel,
 )
 async def get_status(
     request: Request, _: Callable = Depends(jwt_auth_scheme)
@@ -43,13 +43,16 @@ async def get_status(
 async def get_events(
     request: Request, _: Callable = Depends(jwt_auth_scheme)
 ) -> List[EventModel]:
-    events = [{
-        "ts": 1633362463,
-        "severity": "info",
-        "message": "fooo bar asdasdlkasjd aksdjlas dasjdlsakjd asdkasld asdas."
-    }, {
-        "ts": 1633363417,
-        "severity": "warn",
-        "message": "Lorem ipsum dolor sit amet, sed diam voluptua."
-    }]
+    events = [
+        {
+            "ts": 1633362463,
+            "severity": "info",
+            "message": "fooo bar asdasdlkasjd aksdjlas dasjdlsakjd asdkasld asdas.",
+        },
+        {
+            "ts": 1633363417,
+            "severity": "warn",
+            "message": "Lorem ipsum dolor sit amet, sed diam voluptua.",
+        },
+    ]
     return [EventModel.parse_obj(event) for event in events]